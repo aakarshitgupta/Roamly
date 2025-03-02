09f33f7522021b72b6894428c119de81d5c6cf33
Akarshit <aakarshitgupta307@gmail.com>
2025-03-02 19:01:07 +0530

    fixed spelling mistake of joi in schema.js

--- schema.js
+++ schema.js
@@ -1,16 +1,15 @@
 const Joi = require('joi');
-const joi = require('joi');
 
-module.exports.listingSchema = joi.object({
-    listing: joi.object({
-        title: joi.string().required(),
-        description: joi.string().required(),
-        location: joi.string().required(),
-        country: joi.string().required(),
-        price: joi.number().required().min(0),
-        image: joi.string().allow("",null),
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().allow("",null),
 }).required()
-
+    
 })
 
 module.exports.reviewSchema = Joi.object({
